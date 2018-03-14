6e460cc17b34c228b73284bd4335bbcb10edcbf2
Emmanuel Ruaud <eruaud@student.le-101.fr>
2018-03-14 19:09:35 +0100

    tkinter

--- pyviz.py
+++ pyviz.py
@@ -11,7 +11,7 @@
 #                                                        /                     #
 # **************************************************************************** #
 
-import pygame
+from tkinter import *
 import sys
 import time
 import subprocess
@@ -21,10 +21,9 @@ import os
 This python script was created to visualize your work with the PUSH_SWAP
 42 Project.
 You must put this script in the same path or in a sibling path of your program
-Of course you need Python3 with the Pygame Module.
+Of course you need Python3 with the builtin Tkinter.
 You can install it with Brew.
 --> Brew install python3
---> pip3 install pygame
 Execute the script with : 
 --> python3 pyvis.py `ruby -e "puts (-200..200).to_a.shuffle.join(' ')"` 
 You can change the PUSHS_PATH to get to the relative path of your push_swap
@@ -32,119 +31,161 @@ You can decrease the speed by pressing repeatedly the LEFT KEY
 or increase by pressing repeatedly the RIGHT KEY
 '''
 
-SPEED_FACTOR = 3
-RELATIVE_PATH = r'../pushswap/push_swap'
+RELATIVE_PATH = r'push_swap'
 
+class PsGui:
+	def __init__(self, master):
+		self.a_color = 'MediumOrchid4'
+		self.b_color = 'dark slate blue'
+		ww = 600
+		wh = 600
+		self.i = 0
+		self.speed = 0
+		dirname = os.path.dirname(os.path.abspath(__file__))
+		PUSHS_PATH = os.path.join(dirname, RELATIVE_PATH)
+		self.pile_a = [int(num) for num in sys.argv[1:]]
+		self.first_pile = self.pile_a[:]
+		self.pile_b = []
+		cp = subprocess.run([PUSHS_PATH] + sys.argv[1:], stdout=subprocess.PIPE)
+		self.cmds = cp.stdout.splitlines()
+		self.prespeed = len(self.cmds) * 100
+		self.master = master
+		master.title("Push_swap viewer")
+		self.can = Canvas(root, width=ww, height=wh, bg="black")
+		self.can.pack(side=LEFT)
+		self.toolframe = Frame(master)
+		self.toolframe.pack(side=RIGHT, fill=BOTH)
+		self.butframe = Frame(self.toolframe)
+		self.butframe.pack(side=TOP, fill=Y)
+		self.PrevCtl = Button(self.butframe, text="<<", command=self.speed_down)
+		self.PrevCtl.pack(side=LEFT)
+		self.PauseCtl = Button(self.butframe, text=">", command=self.pause)
+		self.PauseCtl.pack(side=LEFT)
+		self.NextCtl = Button(self.butframe, text=">>", command=self.speed_up)
+		self.NextCtl.pack(side=LEFT)
+		self.ResetCtl = Button(self.butframe, text="R", command=self.reset)
+		self.ResetCtl.pack(side=LEFT)
+		self.listbox = Listbox(self.toolframe, bg='black', fg='light cyan')
+		self.listbox.pack(fill=BOTH, expand=1)
+		for cmd in self.cmds:
+			self.listbox.insert(END, cmd)
+		self.draw_rectangles()
+		self.launch()
 
-def display_piles(screen, w_width, w_height, pile_a, pile_b):
-	v_i = 0
-	h_width = w_width / 2
-	hm = len(pile_a) + len(pile_b)
-	mx = max(pile_a + pile_b)
-	mn = min(pile_a + pile_b)
-	if len(pile_a) > 0:
-		a_val = [(num - mn) / (mx - mn) for num in pile_a]
-		for vala in a_val:
-			pygame.draw.rect(screen, (0, 128, 180),
-							 pygame.Rect(0, v_i, 10 + vala * (h_width - 100),
-										 w_height / hm + 1))
-			v_i += w_height / hm
-	v_i = 0
-	if len(pile_b) > 0:
-		b_val = [(num - mn) / (mx - mn) for num in pile_b]
-		for valb in b_val:
-			pygame.draw.rect(screen, (180, 128, 128),
-				pygame.Rect(h_width - 80, v_i, 10 + valb * (h_width - 100),
-					w_height / hm + 1))
-			v_i += w_height / hm
+	def reset(self):
+		self.speed = 0
+		self.i = 0
+		del self.pile_a[:]
+		self.pile_a = self.first_pile[:]
+		del self.pile_b[:]
+		self.can.delete("all")
+		self.draw_rectangles()
+		self.listbox.see(0)
+		self.PauseCtl.config(text='>')
+		self.launch()
 
+	def pause(self):
+		if self.speed != 0:
+			self.prespeed = self.speed
+			self.speed = 0
+			self.PauseCtl.config(text='>')
+		else:
+			self.speed = self.prespeed
+			self.PauseCtl.config(text='||')
 
-def launch_cmds(pile_a, pile_b, cmd):
-	if cmd == b'sa' and len(pile_a) >= 2:
-		pile_a[0], pile_a[1] = pile_a[1], pile_a[0]
-	if cmd == b'sb' and len(pile_b) >= 2:
-		pile_b[0], pile_b[1] = pile_b[1], pile_b[0]
-	if cmd == b'ss' and len(pile_a) >= 2 and len(pile_b) >= 2:
-		pile_a[0], pile_a[1] = pile_a[1], pile_a[0]
-		pile_b[0], pile_b[1] = pile_b[1], pile_b[0]
-	if cmd == b'ra' and len(pile_a) >= 2:
-		pile_a.append(pile_a[0])
-		del pile_a[0]
-	if cmd == b'rb' and len(pile_b) >= 2:
-		pile_b.append(pile_b[0])
-		del pile_b[0]
-	if cmd == b'rr' and len(pile_a) >= 2 and len(pile_b) >= 2:
-		pile_a.append(pile_a[0])
-		del pile_a[0]
-		pile_b.append(pile_b[0])
-		del pile_b[0]
-	if cmd == b'rra' and len(pile_a) >= 2:
-		pile_a = [pile_a[-1]] + pile_a
-		del pile_a[-1]
-	if cmd == b'rrb' and len(pile_b) >= 2:
-		pile_b = [pile_b[-1]] + pile_b
-		del pile_b[-1]
-	if cmd == b'rrr' and len(pile_a) >= 2 and len(pile_b) >= 2:
-		pile_a = [pile_a[-1]] + pile_a
-		del pile_a[-1]
-		pile_b = [pile_b[-1]] + pile_b
-		del pile_b[-1]
-	if cmd == b'pa' and len(pile_b) >= 1:
-		pile_a = [pile_b[0]] + pile_a
-		del pile_b[0]
-	if cmd == b'pb' and len(pile_a) >= 1:
-		pile_b = [pile_a[0]] + pile_b
-		del pile_a[0]
-	return pile_a, pile_b
+	def speed_up(self):
+		if self.speed == 0:
+			self.PauseCtl.config(text='||')
+			self.speed = self.prespeed
+		self.speed *= 10
 
+	def speed_down(self):
+		if self.speed > 10:
+			self.speed /= 10
 
-def main(SPEED_FACTOR):
-	dirname = os.path.dirname(os.path.abspath(__file__))
-	PUSHS_PATH = os.path.join(dirname, RELATIVE_PATH)
-	pygame.init()
-	pygame.display.set_caption('Push_swap viewer')
-	w_width = 800
-	w_height = 600
-	screen = pygame.display.set_mode((w_width, w_height))
-	values = [int(num) for num in sys.argv[1:]]
-	b_values = []
-	cp = subprocess.run([PUSHS_PATH] + sys.argv[1:], stdout=subprocess.PIPE)
-	cmds = cp.stdout.splitlines()
-	myfont = pygame.font.SysFont("monospace", 20)
-	op = 0
-	done = False
-	while not done:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				done = True
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
-					if SPEED_FACTOR > 4:
-						SPEED_FACTOR = SPEED_FACTOR - 4
-				elif event.key == pygame.K_LEFT:
-					SPEED_FACTOR = SPEED_FACTOR + 4
-		screen.fill((0, 0, 0))
-		display_piles(screen, w_width, w_height, values, b_values)
-		if op < len(cmds):
-			pygame.draw.rect(screen, (20, 20, 20),
-				pygame.Rect(650, 0, 200, 800))
-			label = myfont.render(cmds[op], 1, (255, 255, 0))
-			tot = myfont.render('total : ' + str(len(cmds)), 1, (255, 255, 0))
-			screen.blit(label, (710, 20))
-			screen.blit(tot, (690, 40))
-			values, b_values = launch_cmds(values, b_values, cmds[op])
-			op += 1
-			time.sleep(SPEED_FACTOR / len(cmds))
-		elif not all(
-				values[i] <= values[i + 1] for i in range(len(values) - 1)):
-			pygame.draw.rect(screen, (20, 20, 20),
-							 pygame.Rect(650, 0, 200, 800))
-			res = myfont.render('Game Over', 1, (255, 255, 0))
-			screen.blit(res, (700, 60))
-		pygame.display.flip()
-	pygame.quit()
-	sys.exit()
+	def launch_cmds(self, cmd):
+		if cmd == b'sa' and len(self.pile_a) >= 2:
+			self.pile_a[0], self.pile_a[1] = self.pile_a[1], self.pile_a[0]
+		if cmd == b'sb' and len(self.pile_b) >= 2:
+			self.pile_b[0], self.pile_b[1] = self.pile_b[1], self.pile_b[0]
+		if cmd == b'ss' and len(self.pile_a) >= 2 and len(self.pile_b) >= 2:
+			self.pile_a[0], self.pile_a[1] = self.pile_a[1], self.pile_a[0]
+			self.pile_b[0], self.pile_b[1] = self.pile_b[1], self.pile_b[0]
+		if cmd == b'ra' and len(self.pile_a) >= 2:
+			self.pile_a.append(self.pile_a[0])
+			del self.pile_a[0]
+		if cmd == b'rb' and len(self.pile_b) >= 2:
+			self.pile_b.append(self.pile_b[0])
+			del self.pile_b[0]
+		if cmd == b'rr' and len(self.pile_a) >= 2 and len(self.pile_b) >= 2:
+			self.pile_a.append(self.pile_a[0])
+			del self.pile_a[0]
+			self.pile_b.append(self.pile_b[0])
+			del self.pile_b[0]
+		if cmd == b'rra' and len(self.pile_a) >= 2:
+			self.pile_a = [self.pile_a[-1]] + self.pile_a
+			del self.pile_a[-1]
+		if cmd == b'rrb' and len(self.pile_b) >= 2:
+			self.pile_b = [self.pile_b[-1]] + self.pile_b
+			del self.pile_b[-1]
+		if cmd == b'rrr' and len(self.pile_a) >= 2 and len(self.pile_b) >= 2:
+			self.pile_a = [self.pile_a[-1]] + self.pile_a
+			del self.pile_a[-1]
+			self.pile_b = [self.pile_b[-1]] + self.pile_b
+			del self.pile_b[-1]
+		if cmd == b'pa' and len(self.pile_b) >= 1:
+			self.pile_a = [self.pile_b[0]] + self.pile_a
+			del self.pile_b[0]
+		if cmd == b'pb' and len(self.pile_a) >= 1:
+			self.pile_b = [self.pile_a[0]] + self.pile_b
+			del self.pile_a[0]
+		return self.pile_a, self.pile_b
 
+	def draw_rectangles(self):
+		vi = 0
+		ww = 600
+		wh = 600
+		hw = ww / 2
+		hm = len(self.pile_a) + len(self.pile_b)
+		mx = max(self.pile_a + self.pile_b)
+		mn = min(self.pile_a + self.pile_b)
+		rects = []
+		if len(self.pile_a) > 0:
+			a_val = [(num - mn) / (mx - mn) for num in self.pile_a]
+			for vala in a_val:
+				rects.append(self.can.create_rectangle(0, vi,
+					10 + vala * (hw - 100), vi + wh / hm,
+							fill=self.a_color, outline=""))
+				vi += wh / hm
+		vi = 0
+		if len(self.pile_b) > 0:
+			b_val = [(num - mn) / (mx - mn) for num in self.pile_b]
+			for valb in b_val:
+				rects.append(self.can.create_rectangle(hw, vi,
+					hw + 10 + valb * (hw - 100), vi + wh / hm,
+							fill=self.b_color, outline=""))
+				vi += wh / hm
 
-if __name__ == "__main__":
-	main(SPEED_FACTOR)
+	def launch(self):
+		while self.i < len(self.cmds):
+			if self.speed != 0:
+				while self.i < len(self.cmds):
+					self.listbox.activate(self.i)
+					self.can.delete("all")
+					self.pile_a, self.pile_b = \
+						self.launch_cmds(self.cmds[self.i])
+					self.draw_rectangles()
+					time.sleep(0.25 / self.speed)
+					self.can.update()
+					self.listbox.yview_scroll(1, 'units')
+					self.i += 1
+					if self.speed == 0:
+						break
+			time.sleep(0.25)
+			self.can.update()
+		self.PauseCtl.config(text='>')
+
+root = Tk()
+root.resizable(width=False, height=False)
+gui = PsGui(root)
+root.mainloop()
